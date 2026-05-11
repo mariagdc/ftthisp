8cf6604cf60a3e877aff6ac91c4507ee5423fc42
mariagdc <mariagodoydelcastillo@gmail.com>
2026-05-11 09:43:59 -0300

    se agregaron rutas a api.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MapController;
+use App\Http\Controllers\OltController;
+use App\Http\Controllers\OdpController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\FiberCableController;
+use App\Http\Controllers\FaultLogController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +22,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/map', [MapController::class, 'index'])->name('map.index');
+    Route::get('/map-data', [MapController::class, 'data'])->name('map.data');
+    
+    Route::resource('olts', OltController::class);
+    Route::resource('odps', OdpController::class);
+    Route::resource('customers', CustomerController::class);
+    Route::resource('fiber_cables', FiberCableController::class);
+    Route::resource('fault_logs', FaultLogController::class);
+    Route::resource('users', UserController::class);
 });
 
 require __DIR__.'/auth.php';
